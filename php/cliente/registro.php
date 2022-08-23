5233f9594e0622bae65c66e78cd2920860a41991
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-08-23 20:26:09 -0300

    add registro from myfiles

--- php/cliente/registro.php
+++ php/cliente/registro.php
@@ -7,104 +7,78 @@
     require('../headers/menu.php')
 ?>
 
-<h1 class="title">Crear tu cuenta en GT</h1>
-<section>
-    <article class="article">
-        <form action="../php/cliente/registrar.php" method="POST" enctype="multipart/form-data">
+<section class="fondo">
+        <article class="left">
+            <div class="container">
+                <h1 class="title">Bienvenido a <img class="title__logo" src="../../img/login/logo.png" alt="GT"></h1>
+                <form action="../php/cliente_nuevo.php" method="POST" enctype="multipart/form-data">
 
-            <div class="form__item">
-                <label for="usuario"></label>
-                <input type="text" id="usuario" placeholder="usuario" name="usuario" maxlength="60" required>
+                    <input type="text" class="form__item" id="nombre" placeholder="nombre" name="nombre" maxlength="120"
+                        required>
+
+                    <input type="text" class="form__item" id="apellido" placeholder="apellido" name="apellido"
+                        maxlength="120" required>
+
+                    <input type="text" class="form__item" placeholder="43586958" id="dni" placeholder="dni" name="dni"
+                        minlength="8" maxlength="8" required>
+
+                    <input type="date" class="form__item" id="fecha_nac" placeholder="MM/DD/YYYY" name="fecha_nac" required>
+
+                    <!-- <input type="date" class="form__item" id="fecha_nac" placeholder="MM/DD/YYYY" onfocus="(this.type='date')" name="fecha_nac" required>
+                 -->
+
+                    <input type="email" class="form__item" id="mail" placeholder="mail" name="mail" maxlength="80"
+                        required>
+
+                    <input type="text" class="form__item" id="direccion" placeholder="direccion" name="direccion"
+                        maxlength="120" required>
+
+                    <input type="number" class="form__item" id="codigo_postal" placeholder="codigo postal"
+                        name="codigo_postal" maxlength="15" required>
+
+                    <input type="text" class="form__item" id="localidad" placeholder="localidad" name="localidad"
+                        maxlength="100" required>
+ <!--
+                    <input type="text" class="form__item" id="provincia" placeholder="provincia" name="provincia"
+                        maxlength="100" required>
+
+                    <input type="text" class="form__item" id="pais" placeholder="pais" name="pais" maxlength="80"
+                        required>                   
+
+                    <input type="tel" class="form__item" id="telefono" placeholder="telefono" name="telefono" pattern="[0-9]{11}" required>
+
+
+                    <input type="text" class="form__item" id="fax" placeholder="fax" name="fax" maxlength="50">
+
+
+                    <input type="text" class="form__item" id="usuario" placeholder="usuario" name="usuario" maxlength="60" required>
+
+
+                    <input type="password" class="form__item" id="clave" placeholder="clave" name="clave" minlength="6" maxlength="40" required>
+
+
+                    <input type="password" class="form__item" id="confirmar_clave" placeholder="confirmar clave" name="confirmar_clave" required>
+
+
+                    <input type="file" class="form__item" id="foto_usuario" placeholder="foto usuario" name="foto"> -->
+
+                    <div class="siguiente">
+                        <img src="../../img/login/siguiente.png" class="siguiente_img" alt="Siguiente">
+                    </div>
+
+                    <!-- Botones -->
+                    <!-- <input id="btn-registro" type="submit" value="Registrarme">
+                    <a href="../index.php">
+                        <input id="btn-cancelar" type="button" value="Cancelar">
+                    </a>
+
+                    <input type="hidden" value="comun" name="tipo_usuario"> -->
+                </form>
             </div>
+        </article>
+        <img src="../../img/login/flecha.png" alt="flecha" class="flecha">
+    </section>
 
-            <div class="form__item">
-                <label for="clave"></label>
-                <input type="password" id="clave" placeholder="clave" name="clave" minlength="6" maxlength="40" required>
-            </div>
-
-            <div class="form__item">
-                <label for="confirmar_clave"></label>
-                <input type="password" id="confirmar_clave" placeholder="confirmar clave" name="confirmar_clave" required>
-            </div>
-
-            <div class="form__item">
-                <label for="nombre"></label>
-                <input type="text" id="nombre" placeholder="nombre" name="nombre" maxlength="120" required>
-            </div>
-
-            <div class="form__item">
-                <label for="apellido"></label>
-                <input type="text" id="apellido" placeholder="apellido" name="apellido" maxlength="120" required>
-            </div>
-
-            <div class="form__item">
-                <label for="dni"></label>
-                <input type="text" placeholder="43586958" id="dni" placeholder="dni" name="dni" minlength="8" maxlength="8" required>
-            </div>
-
-            <div class="form__item">
-                <label for="fecha_nac"></label>
-                <input type="date" id="fecha_nac" placeholder="fecha_nac" name="fecha_nac" required>
-            </div>
-
-            <div class="form__item">
-                <label for="mail"></label>
-                <input type="email" id="mail" placeholder="mail" name="mail" maxlength="80" required>
-            </div>
-
-            <div class="form__item">
-                <label for="direccion"></label>
-                <input type="text" id="direccion" placeholder="direccion" name="direccion" maxlength="120" required>
-            </div>
-
-            <div class="form__item">
-                <label for="codigo_postal"></label>
-                <input type="number" id="codigo_postal" placeholder="codigo postal" name="codigo_postal" maxlength="15" required>
-            </div>
-
-            <div class="form__item">
-                <label for="localidad"></label>
-                <input type="text" id="localidad" placeholder="localidad" name="localidad" maxlength="100" required>
-            </div>
-
-            <div class="form__item">
-                <label for="provincia"></label>
-                <input type="text" id="provincia" placeholder="provincia" name="provincia" maxlength="100" required>
-            </div>
-
-            <div class="form__item">
-                <label for="pais"></label>
-                <input type="text" id="pais" placeholder="pais" name="pais" maxlength="80" required>
-            </div>
-
-            <div class="form__item">
-                <label for="telefono"></label>
-                <input type="tel" id="telefono" placeholder="telefono" name="telefono" pattern="[0-9]{11}" required>
-            </div>
-
-            <div class="form__item">
-                <label for="fax"></label>
-                <input type="text" id="fax" placeholder="fax" name="fax" maxlength="50">
-            </div>
-
-            <div class="form__item">
-                <label for="foto_usuario"></label>
-                <input type="file" id="foto_usuario" placeholder="foto_usuario" name="foto">
-            </div>
-
-            <!-- Botones -->
-            <input id="btn-registro" type="submit" value="Registrarme">
-            <a href="../index.php">
-                <input id="btn-cancelar" type="button" value="Cancelar">
-            </a>
-
-            <input type="hidden" value="comun" name="tipo_usuario">
-        </form>
-    </article>
-    <!-- <article>
-        <img src="../../img/form_nuevo/logo.jpg" alt="">
-    </article> -->
-</section>
 
 <?php
 require('../footer.php');
